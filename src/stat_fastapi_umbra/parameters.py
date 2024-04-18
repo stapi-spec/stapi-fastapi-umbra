e682ce92927379bd69346455c2ea462e692d864f
CloudNiner <andy@finkmiller.com>
2024-04-18 14:54:21 +0200

    Update Products

---
+++ src/stat_fastapi_umbra/parameters.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from enum import StrEnum
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+
+class ProductType(StrEnum):
+    GEC = "GEC"
+    SIDD = "SIDD"
+    SICD = "SICD"
+
+
+DEFAULT_PRODUCT_TYPES: list[ProductType] = [ProductType.GEC, ProductType.SICD]
+
+
+class SceneSize(StrEnum):
+    FIVE_BY_FIVE = "5x5_KM"
+    TEN_BY_TEN = "10x10_KM"
+
+class SatelliteID(StrEnum):
+    UMBRA_04 = "Umbra-04"
+    UMBRA_05 = "Umbra-05"
+    UMBRA_07 = "Umbra-07"
+    UMBRA_08 = "Umbra-08"
+
+
+DEFAULT_SATELLITE_IDS = [e.value for e in SatelliteID]
+
+
+class UmbraSpotlightParameters(BaseModel):
+    """Umbra Spotlight Parameters JSON Schema"""
+    sceneSize: SceneSize = Field(title="Scene Size", default=SceneSize.FIVE_BY_FIVE, description="The scene size of the Spotlight image.")
+    grazingAngleDegrees: int = Field(title="Grazing Angle in Degrees", description="The minimum angle between the local tangent plane at the target location and the line of sight vector between the satellite and the target.", default=45, ge=40, le=70)
+    satelliteIds: list[SatelliteID] = Field(title="Satellite ID", description="The satellites to consider for this Opportunity. See https://docs.canopy.umbra.space/docs/umbra-satellites", default=DEFAULT_SATELLITE_IDS)
+    deliveryConfigId: UUID | None = Field(title="Delivery Config ID", description="https://docs.canopy.umbra.space/docs/delivery-configs", default=None)
+    productTypes: list[ProductType] = Field(title="Product Types", description="https://docs.canopy.umbra.space/docs/delivered-product-types", default=DEFAULT_PRODUCT_TYPES)
+
+
+class UmbraArchiveParameters(BaseModel):
+    pass
--- src/stat_fastapi_umbra/products.py
+++ src/stat_fastapi_umbra/products.py
@@ -8,40 +8,43 @@ from pydantic import BaseModel
 from stat_fastapi.models.product import Product, Provider, ProviderRole
 from stat_fastapi.models.shared import Link
 
+from .parameters import UmbraArchiveParameters, UmbraSpotlightParameters
+
 
 class SceneSizeConstraints(Enum):
     """Scene Size Contraints"""
-
     SCENE_5X5_KM = "5x5_KM"
     SCENE_10X10_KM = "10x10_KM"
 
 
 class SpotlightConstraints(BaseModel):
     """Spotlight Constraints"""
-
     scene_size: SceneSizeConstraints
 
-
-SPOTLIGHT_PRODUCT = Product(
-    id="umbra_spotlight",
-    title="Umbra spotlight SAR capture",
-    description="Umbra Spotlight Product",
-    keywords=["sar", "radar", "umbra"],
-    license="CC0-1.0",
-    providers=[
-        Provider(
-            name="UMBRA",
+umbra_provider = Provider(
+            name="Umbra",
+            description="Global Omniscience",
             roles=[
                 ProviderRole.licensor,
                 ProviderRole.producer,
                 ProviderRole.processor,
                 ProviderRole.host,
             ],
-            url="http://umbra.space",
+            url="https://umbra.space",
         )
-    ],
-    links=[],
-    constraints=SpotlightConstraints,
+
+canopy_docs_link = Link(href="https://docs.canopy.umbra.space", rel="documentation", type="docs", title="Canopy Documentation")
+
+
+SPOTLIGHT_PRODUCT = Product(
+    id="umbra_spotlight",
+    title="Umbra Spotlight Task",
+    description="Spotlight images served by creating new Orders.",
+    keywords=["sar", "radar", "umbra", "spotlight"],
+    license="CC-BY-4.0",
+    providers=[umbra_provider],
+    links=[canopy_docs_link],
+    parameters=UmbraSpotlightParameters,
 )
 
 ARCHIVE_PRODUCT = Product(
@@ -49,22 +52,9 @@ ARCHIVE_PRODUCT = Product(
     title="Umbra Archive Catalog",
     description="Umbra SAR Images served by the Archive Catalog.",
     keywords=["sar", "radar", "umbra", "catalog", "archive"],
-    license="CC0-1.0",
-    providers=[
-        Provider(
-            name="UMBRA",
-            roles=[
-                ProviderRole.licensor,
-                ProviderRole.producer,
-                ProviderRole.processor,
-                ProviderRole.host,
-            ],
-            url="http://umbra.space",
-        )
-    ],
-    links=[
-        Link(href="https://api.canopy.umbra.space/archive/search", rel="catalog"),
-    ],
-    constraints=SpotlightConstraints,
+    license="CC-BY-4.0",
+    providers=[umbra_provider],
+    links=[canopy_docs_link],
+    parameters=UmbraArchiveParameters,
 )
 PRODUCTS = [SPOTLIGHT_PRODUCT, ARCHIVE_PRODUCT]
